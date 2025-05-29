6ccc261c4266b49e6925958ea493b2f1668e0f1c
Fredrik000 <fredrik.skogstad94@gmail.com>
2025-05-29 05:36:30 +0200

    react query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,19 @@
 import { MantineProvider } from '@mantine/core';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+// Create a client
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <MantineProvider>
-        <Outlet />
-        <TanStackRouterDevtools />
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+          <TanStackRouterDevtools />
+        </QueryClientProvider>
       </MantineProvider>
     </>
   ),
